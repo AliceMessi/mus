684228043101f4b0aa38edd6c7383f038b87ec95
AliceMessi <alicemessi26@gmail.com>
2026-03-24 05:48:45 +0100

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2449952.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 280574531, 280574244, 280574226, 280574240, 280574234, 280806183, 280807660, 280824359 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : '', 'wasmUrl' : 'files/assets/280574239/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/280574241/1/ammo.js', 'preload' : true},
+];
